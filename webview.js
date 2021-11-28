9c063d72c44d6b57829a79785ac9de89ec711fff
deadmeu <deadmeu@gmail.com>
2021-11-28 18:46:01 +1000

    Implement badges for direct and indirect counts

--- webview.js
+++ webview.js
@@ -1,9 +1,18 @@
-"use strict";
-
 module.exports = Ferdi => {
-  const getMessages = function getMessages() {
-    // TODO: Insert your notification-finding code here
-    Ferdi.setBadge(0, 0);
+  const getMessages = () => {
+    let directCount = 0;
+    let indirectCount = 0;
+
+    const messageCountElement = document.getElementById('Message-umi');
+    if (messageCountElement) {
+      directCount = parseInt(messageCountElement.textContent) || 0;
+    }
+
+    const unreadChats = document.querySelectorAll('.has-unread');
+    // unreadChats includes direct messages - do not count them
+    indirectCount = unreadChats.length - directCount;
+
+    Ferdi.setBadge(directCount, indirectCount);
   };
 
   Ferdi.loop(getMessages);
